886d7e4d93ef0c221305230fed3119821731841d
agentromain <romain.artru@epfl.ch>
2017-07-27 15:06:00 +0200

    wip before trying oauth

--- frog/imports/api/public/api.js
+++ frog/imports/api/public/api.js
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
 import { Picker } from 'meteor/meteorhacks:picker';
 
+Picker.middleware(bodyParser.urlencoded({ extended: false }));
 Picker.middleware(bodyParser.json());
+
 Picker.filter((req,res) => req.method === 'POST')
-.route('/api/webhooks/:provider', ({ Provieder }, request, response) => {
+.route('/api/webhooks/:provider', ({Provider}, request, response) => {
  // We'll handle the request here.`
 
  let isLTI = request.body.lti_message_type === 'basic-lti-launch-request' &&
@@ -14,11 +16,11 @@ Picker.filter((req,res) => req.method === 'POST')
  let isValid = request.body.oauth_callback === 'about:blank' && request.body.oauth_version === '1.0';
  //check authenticity with a oauth library
 
+ console.log(request.body);
  if(isLTI){
    if(isValid) console.log('valid LTI launch request');
    else console.log('unvalid LTI launch request');
  }
-
 
  response.end('Hello');
 });
